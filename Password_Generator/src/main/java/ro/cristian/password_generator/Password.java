7c7c8ef230a8ea7f21b44591bc54e7bfbface11a
Cristian-Sebastian Csete <cristiancsete@yahoo.com>
2023-03-31 19:40:57 +0100

    Generator update
    -- Added feature to have at least one of each selected things
    -- Changed the appending into a StringBuilder to be more efficient

--- Password_Generator/src/main/java/ro/cristian/password_generator/Password.java
+++ Password_Generator/src/main/java/ro/cristian/password_generator/Password.java
@@ -1,18 +1,19 @@
 package ro.cristian.password_generator;
 
-import java.util.Random;
+import java.util.*;
 
 public class Password {
-    private int length;
-    private boolean lowerCase;
-    private boolean upperCase;
-    private boolean numbers;
-    private boolean sCharacters;
+    private final int length;
+    private final boolean lowerCase;
+    private final boolean upperCase;
+    private final boolean numbers;
+    private final boolean sCharacters;
+    private final Random random = new Random();
 
-    private String[] lowerCasePosible = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
-    private String[] upperCasePosible = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
-    private String[] numbersPosible = {"0","1","2","3","4","5","6","7","8","9"};
-    private String[] sCharactersPosible = {"'","!","@","#","%","^","&","*","(",")","-","=","+","_","<",">",".","?","/"};
+    private final List<Character> lowerCasePossible = new LinkedList<>(List.of('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'));
+    private final List<Character> upperCasePossible = new LinkedList<>(List.of('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'));
+    private final List<Character> numbersPossible = new LinkedList<>(List.of('0','1','2','3','4','5','6','7','8','9'));
+    private final List<Character> sCharactersPossible = new LinkedList<>(List.of('`', '"','\'','!','@','#','%','^','&','*','(',')','-','=','+','_','<','>','.','?','/'));
 
     public Password(int length, boolean lowerCase, boolean upperCase, boolean numbers, boolean sCharacters){
         this.length = length;
@@ -23,48 +24,49 @@ public class Password {
     }
 
     protected String generatePassword(){
-        String password = "";
-
-        int currentPos = 0;
-        String[] posibleCharacters = new String[100];
+        StringBuilder password = new StringBuilder();
+        List<Character> possibleCharacters = new LinkedList<>();
 
         if(lowerCase){
-            for(int i=0; i< lowerCasePosible.length; i++){
-                posibleCharacters[currentPos]=lowerCasePosible[i];
-                currentPos++;
-            }
+            add(password, lowerCasePossible);
+            possibleCharacters.addAll(lowerCasePossible);
         }
 
         if(upperCase){
-            for(int i=0; i< upperCasePosible.length; i++){
-                posibleCharacters[currentPos]=upperCasePosible[i];
-                currentPos++;
-            }
+            add(password, upperCasePossible);
+            possibleCharacters.addAll(upperCasePossible);
         }
 
         if(numbers){
-            for(int i=0; i< numbersPosible.length; i++){
-                posibleCharacters[currentPos]=numbersPosible[i];
-                currentPos++;
-            }
+            add(password, numbersPossible);
+            possibleCharacters.addAll(numbersPossible);
         }
 
         if(sCharacters){
-            for(int i=0; i< sCharactersPosible.length; i++){
-                posibleCharacters[currentPos]=sCharactersPosible[i];
-                currentPos++;
-            }
+            add(password, sCharactersPossible);
+            possibleCharacters.addAll(sCharactersPossible);
         }
+        
+        while(password.length() < length) add(password, possibleCharacters);
 
-        for(int i=1; i<=length; i++){
-            Random r = new Random();
-            int x = r.nextInt(currentPos-1);
-            password = password.concat(posibleCharacters[x]);
-        }
+        //Shuffle the characters for more randomness
+        List<Character> shuffle = new ArrayList<>();
+        for(int i = 0; i < password.length(); i++) shuffle.add(password.charAt(i));
+        Collections.shuffle(shuffle);
+        password.delete(0, password.length());
+        for(char c : shuffle) password.append(c);
+
+        //Delete if the password is too long
+        //User might want 4 types of chars in a length of 3 which is not possible
+        if(password.length() - length > 0) password.delete(0, password.length() - length);
 
         //TODO: make sure that each type of character is used at least once
+        return password.toString();
+    }
 
-        return password;
+    private void add(StringBuilder builder, List<Character> list){
+        int posAdd = random.nextInt(list.size());
+        builder.append(list.get(posAdd));
     }
 
 }
